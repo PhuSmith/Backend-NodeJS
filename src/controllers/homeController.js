c122ad3226f5d05cce63bf6dbcaddb3dff91ffe7
Phu <duongquocphu1996@gmail.com>
2021-12-20 13:11:26 +0700

    Add user to DB

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,4 +1,5 @@
 import db from "../models/index";
+import CRUDService from "../services/CRUDService";
 
 let getHomePage = async (req, res) => {
   try {
@@ -12,4 +13,14 @@ let getHomePage = async (req, res) => {
 let getAboutPage = (req, res) => {
   return res.render("test/about.ejs");
 };
-module.exports = { getHomePage, getAboutPage };
+
+let getCRUD = (req, res) => {
+  return res.render("crud.ejs");
+};
+
+let postCRUD = async (req, res) => {
+  let message = await CRUDService.createNewUser(req.body);
+  console.log(message);
+  return res.send("post crud from server");
+};
+module.exports = { getHomePage, getAboutPage, getCRUD, postCRUD };
