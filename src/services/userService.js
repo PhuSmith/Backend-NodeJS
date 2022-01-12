c12bea2e5bca136d8ddf11828498cb02ddb591dd
Phu <duongquocphu1996@gmail.com>
2022-01-12 13:02:47 +0700

    Update DB type User

--- src/services/userService.js
+++ src/services/userService.js
@@ -120,9 +120,11 @@ let createNewUser = (data) => {
           firstName: data.firstName,
           lastName: data.lastName,
           address: data.address,
-          phonenumber: data.phonenumber,
-          gender: data.gender === '1' ? true : false,
-          roleId: data.roleId,
+          phonenumber: data.phoneNumber,
+          gender: data.gender,
+          roleId: data.role,
+          positionId: data.position,
+          image: data.avatar,
         });
         resolve({
           errCode: 0,
@@ -163,7 +165,7 @@ let deleteUser = (userId) => {
 let updateUserData = (data) => {
   return new Promise(async (resolve, reject) => {
     try {
-      if (!data.id) {
+      if (!data.id || !data.role || !data.position || !data.gender) {
         resolve({
           errCode: 1,
           errMessage: 'Missing required parameter!',
@@ -177,6 +179,14 @@ let updateUserData = (data) => {
         (user.firstName = data.firstName),
           (user.lastName = data.lastName),
           (user.address = data.address);
+        (user.roleId = data.role),
+          (user.positionId = data.position),
+          (user.gender = data.gender),
+          (user.phonenumber = data.phoneNumber);
+        if (data.avatar) {
+          user.image = data.avatar;
+        }
+
         await user.save();
         resolve({
           errCode: 0,
