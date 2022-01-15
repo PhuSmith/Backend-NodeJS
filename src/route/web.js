ce3e3a7d1f58b07a6b57e1909b5289de48da13f8
Phu <duongquocphu1996@gmail.com>
2022-01-15 08:23:49 +0700

    Create GetAllDoctor Api

--- src/route/web.js
+++ src/route/web.js
@@ -25,6 +25,9 @@ let initWebRoutes = (app) => {
 
   router.get('/api/top-doctor-home', doctorController.getTopDoctorHome);
 
+  router.get('/api/get-all-doctors', doctorController.getAllDoctors);
+  router.post('/api/save-infor-doctor', doctorController.postInforDoctor);
+
   return app.use('/', router);
 };
 module.exports = initWebRoutes;
--- src/services/doctorService.js
+++ src/services/doctorService.js
@@ -36,4 +36,54 @@ let getTopDoctorHome = (limit) => {
   });
 };
 
-module.exports = { getTopDoctorHome };
+let getAllDoctors = () => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      let doctors = await db.User.findAll({
+        where: { roleId: 'R2' },
+        attributes: {
+          exclude: ['password', 'image'],
+        },
+      });
+      resolve({
+        errCode: 0,
+        data: doctors,
+      });
+    } catch (e) {
+      reject(e);
+    }
+  });
+};
+
+let saveDetailInforDoctor = (inputData) => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      console.log(inputData);
+      if (
+        !inputData.doctorId ||
+        !inputData.contentHTML ||
+        !inputData.contentMarkdown
+      ) {
+        resolve({
+          errCode: 1,
+          errMessage: 'Missing parameter',
+        });
+      } else {
+        await db.Markdown.create({
+          contentHTML: inputData.contentHTML,
+          contentMarkdown: inputData.contentMarkdown,
+          description: inputData.description,
+          doctorId: inputData.doctorId,
+        });
+        resolve({
+          errCode: 0,
+          errMessage: "Save infor doctor's success",
+        });
+      }
+    } catch (e) {
+      reject(e);
+    }
+  });
+};
+
+module.exports = { getTopDoctorHome, getAllDoctors, saveDetailInforDoctor };
